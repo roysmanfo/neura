a8b8418f90ab6ef92a09523d9c0a0a89a129a9ac
RoysManfo <manforoys@gmail.com>
2024-05-27 00:24:16 +0200

    Fix input

--- nnfs/layers/dense.py
+++ nnfs/layers/dense.py
@@ -1,6 +1,7 @@
 from typing import List
 from nnfs.layers import Layer
 from nnfs.nodes import Node
+from nnfs.utils.types import InputValue
 
 class Dense(Layer):
     def __eq__(self, __value: object) -> bool:
@@ -12,13 +13,13 @@ class Dense(Layer):
         res = node.calc(x) + self.bias
         return res
 
-    def calc(self, x: List[float]) -> List[float]:
+    def calc(self, x: InputValue) -> List[float]:    
         if not isinstance(x, (list, tuple)):
             raise ValueError("incompatible type: expected list or tuple, received:", type(x).__name__)
 
         v: List[float] = []
 
         for node in self.nodes:
-            v.append(self._analize(x, node))
+            v.append(self._analize(x, node)) # type: ignore
 
         return v
