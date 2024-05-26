e9901f06a587dd0075723962a4aab7a427be67ad
Roys Manfo <94235711+roysmanfo@users.noreply.github.com>
2024-05-26 18:04:16 +0000

    sprt

--- nnfs/activation/__init__.py
+++ nnfs/activation/__init__.py
@@ -1,12 +1,28 @@
 """
 Available activation functions
 """
+
+
+__all__ = (
+    'ActivationFunction',
+    'Exponential',
+    'LeakyReLu',
+    'Linear',
+    'PReLU',
+    'ReLu',
+    'Sigmoid',
+    'Swish',
+    'Tanh'
+)
+
+
 from .activation import ActivationFunction
-from .activation import Linear
-from .activation import PReLU
 from .activation import Exponential
 from .activation import LeakyReLu
+from .activation import Linear
+from .activation import PReLU
 from .activation import ReLu
 from .activation import Sigmoid
-from .activation import Tanh
 from .activation import Swish
+from .activation import Tanh
+
