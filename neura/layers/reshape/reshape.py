f885495c811ff129e43c21bef5b918bb2d6325f1
roysmanfo <manforoys@gmail.com>
2024-08-14 23:41:34 +0200

    remove padding options

--- neura/layers/reshape/reshape.py
+++ neura/layers/reshape/reshape.py
@@ -7,21 +7,10 @@ from neura.utils.types import InputValue, OutputValue
 class Reshape(Layer):
     """
     Reshape the input tensor into the `new_shape` (if possible).
-
-    When there aren't enough values, and `pad` is True, the layer will  
-    add additional zeros in order to match the needed shape,  
-    and `order` will determine where
-
-    - start: The zeros will be added at the start of the tensor
-    - end: The zeros will be added at the end of the tensor
-    - even: The zeros will be added evently between start and end
-    
     """
 
     def __init__(self,
                  new_shape: tuple[int, ...],
-                 pad: bool = False,
-                 order: Literal["start", "end", "even"] = "end",
                  **kwargs: dict[str, Any]
                  ) -> None:
         super().__init__(
@@ -33,8 +22,6 @@ class Reshape(Layer):
             )
         
         self.new_shape = new_shape
-        self.pad = pad
-        self.order = order
 
         self.trainable = False
         self.all_input_at_once = True
@@ -47,11 +34,5 @@ class Reshape(Layer):
         if not isinstance(self.new_shape, tuple) or not "".join([str(i) for i in self.new_shape]).isnumeric():
             raise ValueError("new shape must be a tuple of ints")
         
-        if not isinstance(self.pad, bool):
-            raise ValueError("pad must be a bool")
-        
-        if not isinstance(self.order, str) or not self.order in ("start", "end", "even"):
-            raise ValueError("order must be either 'start', 'end' or 'even' ")
-
     def forward(self, x: InputValue) -> OutputValue:
         return x.reshape(self.new_shape)
